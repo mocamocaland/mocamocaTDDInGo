779b0a629057b0d09f2e146eb0fac2b458b40b84
mocamocaland <noi_kaas@yahoo.co.jp>
2023-05-21 19:16:32 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mocamocaTDDInGo
+
+go 1.20
---
+++ input/parser.go
@@ -0,0 +1,16 @@
+package input
+
+import (
+	"mocamocaTDDInGo/calculator"
+)
+
+caluculator
+
+type Parser struct {
+	engine *calculator.Engine
+	validator *Validator
+}
+
+func (p *Parser) ProcessExpression(expr string) (*string, error) {
+	// implementation code
+}
