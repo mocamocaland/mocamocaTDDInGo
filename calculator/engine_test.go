8ae694c0c1865583343aef2b8b8995bd8dc0b77c
mocamocaland <noi_kaas@yahoo.co.jp>
2023-05-26 01:33:39 +0900

    write testdata, expected ,act

--- calculator/engine_test.go
+++ calculator/engine_test.go
@@ -2,20 +2,61 @@ package calculator_test
 
 import (
 	"github.com/mocamocaland/tdd-in-go-sample/calculator"
+	"log"
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	// setup statements
+	setup()
+	// run the tests
+	e := m.Run()
+
+	teardown()
+	// report the exit code
+	os.Exit(e)
+}
+
+func setup() {
+	log.Println("Setting up.")
+}
+
+func teardown() {
+	log.Println("Tearing down.")
+}
+
+func init() {
+	log.Println("Init setup.")
+}
+
 func TestAdd(t *testing.T) {
+	//defer func() {
+	//	log.Println("Deferred tearig down.")
+	//}()
 	// Arrange
 	e := calculator.Engine{}
-	x, y := 2.5, 3.5
-	want := 6.0
 
-	// Act
-	got := e.Add(x, y)
+	actAssert := func(x, y, want float64) {
 
-	// Assert
-	if got != want {
-		t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want:%.2f", 2.5, 3.5, got, 6.0)
+		// Act
+		got := e.Add(x, y)
+
+		// Assert
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want:%.2f", x, y, got, want)
+		}
 	}
+	// テストデータ、期待値、検証
+	t.Run("posivite input", func(t *testing.T) {
+		x, y := 2.5, 3.5
+		want := 6.0
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -2.5, -3.5
+		want := -6.0
+		actAssert(x, y, want)
+	})
 }
